18c7d1f4f0a7899c039ee4ab5bf7d9fc72ceeff5
zhengxuping <xupingzheng@126.com>
2017-10-08 18:39:40 +0800

    增加不按维度直接读取所有数据的函数

--- DeployC++/Source/ReadMat.cpp
+++ DeployC++/Source/ReadMat.cpp
@@ -31,10 +31,44 @@ cv::Mat DataRead(const std::string& filename, const std::string& matname)
     return ReadMat;
 }
 
-int main()
+cv::Mat DataRead2(const std::string& filename, const std::string& matname)
+{
+    MATFile *pmatFile = matOpen(filename.c_str(), "r");
+    if (pmatFile == NULL)
+        std::cout << "MatOpen error!!!" << std::endl;
+    mxArray *pMxArray = matGetVariable(pmatFile, matname.c_str()); //从文件中获取数据到mxArray结构中  
+    if (pMxArray == NULL)
+        std::cout << "Error reading existing matrix " << matname << "!!!" << std::endl;
+    int numDims = mxGetNumberOfDimensions(pMxArray);
+    const size_t* dims = mxGetDimensions(pMxArray);
+    int numVals = 1;
+    for (int i = 0; i < numDims; i++)
+        numVals *= dims[i];
+    std::vector<int> matDims(numDims);
+    for (int i = 0; i < numDims; i++)
+        matDims[i] = dims[i];
+    double *ReadArray = (double*)mxGetData(pMxArray);
+    cv::Mat ReadMat(1, numVals, CV_64FC1, ReadArray);  //此处主要是自己工程其他模块需使用float型的  
+    mxDestroyArray(pMxArray);
+    if (matClose(pmatFile) != 0)
+        std::cout << "Error closing file " << pmatFile << std::endl;
+    std::cout << "Read done!!!" << std::endl;
+    return ReadMat;
+}
+
+int main1()
 {
     cv::Mat a = DataRead("E:\\Projects\\SRCNN\\Deploy\\model\\9-1-5(91 images)\\x3.mat", "weights_conv1");
     std::ofstream f("f1");
+    f << a << std::endl;
+    f.close();
+    return 0;
+}
+
+int main()
+{
+    cv::Mat a = DataRead2("E:\\Projects\\SRCNN\\3-4-5.mat", "mat");
+    std::ofstream f("f2");
     f << a << std::endl;
     f.close();
     return 0;
